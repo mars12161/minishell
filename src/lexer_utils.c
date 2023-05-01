9862b4969c75901902ba1278f1b73a46c9887cc6
Yuanying Li <yli@c4a4c9.42berlin.de>
2023-05-01 21:54:00 +0200

    lexer

--- src/lexer_utils.c
+++ src/lexer_utils.c
@@ -1,14 +1,5 @@
 #include "../includes/minishell.h"
 
-char	*ft_strchr(const char*str, int c) //from libft, delete later
-{
-	while (*str != (char)c && *str)
-		str++;
-	if ((char)c !='\0' && *str == '\0')
-		return (NULL);
-	return ((char *)str);
-}
-
 void	free_exit_lexer(t_lexer **lexer)
 {
 	t_lexer	temp;
