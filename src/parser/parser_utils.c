dca4b96d682e1555b23b99d1b84e9f3dafc26525
mars12161 <mars12161@gmail.com>
2023-05-12 16:04:17 +0200

    small changes to parser

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,27 +6,19 @@
 /*   By: mschaub <mschaub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:32:52 by mschaub           #+#    #+#             */
-/*   Updated: 2023/05/08 15:40:33 by mschaub          ###   ########.fr       */
+/*   Updated: 2023/05/12 15:40:20 by mschaub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "parser.h"
 
-t_parser	init_parser(t_shell *list)
-{
-	t_parser	parser;
-
-	parser.list = list;
-	parser.redirections = NULL;
-	return (parser);
-}
 
 /*
  Function to count all the arguments
  separated by a Pipe
  */
-int	count_args(t_shell *list)
+int		count_args(t_shell *list)
 {
 	t_shell	*tmp;
 	int		i;
@@ -73,24 +65,4 @@ t_shell	*get_next_pipe(t_shell *list)
 	while (tmp && tmp->type != PIPE)
 		tmp = tmp->next;
 	return (tmp);
-}
-
-void	del_node(t_shell **list, t_shell *node)
-{
-	t_shell	*current;
-
-	if (*list == node)
-	{
-		*list = node->next;
-		free(node);
-		return ;
-	}
-	current = *list;
-	while (current->next && current->next != node)
-		current = current->next;
-	if (current->next == node)
-	{
-		current->next = node->next;
-		free(node);
-	}
 }
