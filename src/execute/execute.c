df68b14a9ab4f2f34c1a03340228157d59756416
Yuanying Li <yli@c4a6c10.42berlin.de>
2023-05-14 19:18:28 +0200

    access

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -38,7 +38,7 @@ char    **ft_get_path(t_parse *parse, t_env **env)
         }
         temp = temp->next;
     }
-    ft_error("did not find the path\n");
+    ft_error("did not find the path");
     return (NULL);
 }
 
@@ -64,22 +64,34 @@ char	*ft_strjoin_path_cmd(char *s1, char c, char *s2)
 	str[i] = '\0';
 	return (str);
 }
+/*
+int	ft_error(char *str)
+{
+	int	i;
 
+	i = 0;
+    while (str[i])
+		i++;
+	write(2, str, i);
+	write(2, "\n", 1);
+    return (-1);
+}
+*/
 char	**ft_check_cmd(char *cmd)
 {
 	char	**command;
 
 	if (cmd == 0)
-		ft_perror("0");
+		ft_error("empty command");
 	else if (cmd[0] == '\0')
-		ft_perror("0");
+		ft_error("empty command");
 	command = ft_split(cmd, ' ');
 	if (command == 0)
-		ft_perror("1");
+		ft_error("empty command");
 	return (command);
 }
 
-void	ft_executer(char *cmd, char **envp)
+int	ft_executer(char *cmd, char **envp)
 {
 	char	**command;
 	char	**path;
@@ -89,7 +101,7 @@ void	ft_executer(char *cmd, char **envp)
 	command = ft_check_cmd(cmd);
 	path = split_path(envp);
 	if (path[0] == NULL)
-		ft_perror("2");
+		ft_error("empty command");
 	i = 0;
 	while (path[i])
 	{
@@ -97,74 +109,14 @@ void	ft_executer(char *cmd, char **envp)
 		if (access(path_cmd, F_OK) != -1)
 		{
 			if (execve(path_cmd, command, NULL) == -1)
-				ft_perror("3");
+				ft_perror("execute failed");
 		}
+		else
+			ft_error("command not found");
 		free (path_cmd);
 		i++;
 	}
 	check_free_all(command);
 	check_free_all(path);
-	ft_perror("4");
-}
-
-
-int ft_unset(t_builtin *node, t_env **env)
-{
-    t_env *temp;
-    char *result;
-    if (node->size == 1)
-        return (ft_error("unset: not enough arguments"));
-    temp = *env;
-    while (temp)
-    {
-        if (!strncmp(temp->content, node->command[1], ft_strlen(node->command[1])))
-        {
-            del_node(env,temp);
-            return (0);
-        }
-        temp = temp->next;
-    }
-    //ft_env(env);
-    return (0);
-}
-
-char	**split_path(char **envp)
-{
-	char	**path;
-	int		i;
-
-	i = 0;
-	while (envp[i])
-	{
-		if (!strncmp(envp[i], "PATH=", 5))
-		{
-			path = ft_split(envp[i] + 5, ':');
-			return (path);
-		}
-		i++;
-	}
-	return (NULL);
-}
-
-char	*ft_strjoin_path_cmd(char const *s1, char c, char const *s2)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-
-	if (!s1 || !s2)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[j])
-		str[i++] = s1[j++];
-	str[i++] = c;
-	j = 0;
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	return (str);
+	ft_error("execute failed");
 }
