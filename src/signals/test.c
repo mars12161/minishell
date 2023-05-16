962c3285879366ad86d42940a2439db0e72fe9a4
mars12161 <108153152+mars12161@users.noreply.github.com>
2023-05-16 17:15:59 +0200

    additions to signals

--- src/signals/test.c
+++ src/signals/test.c
@@ -6,11 +6,13 @@
 /*   By: mschaub <mschaub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 13:17:01 by mschaub           #+#    #+#             */
-/*   Updated: 2023/05/16 13:52:00 by mschaub          ###   ########.fr       */
+/*   Updated: 2023/05/16 17:08:53 by mschaub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signals.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -24,19 +26,27 @@ int	ft_strcmp(char *s1, char *s2)
 
 int	minishell_loop(void)
 {
-	char *input;
+	char	*input;
+
 	while (42)
 	{
 		input = readline("=> ");
-		signal(SIGINT, ft_sighandler);
-		signal(SIGQUIT, ft_sighandler);
+		//signal(SIGINT, ft_sighandler);
+		//signal(SIGQUIT, ft_sighandler);
 		if (!ft_strcmp(input, "exit"))
 			return (1);
 		printf("%s\n", input);
 	}
 }
 
-int main(void)
+int	main(void)
 {
-		minishell_loop();
+	char	*input;
+
+	input = readline("Enter your name: ");
+	//rl_replace_line("Hello", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	printf("Hello %s\n", input);
+
 }
